ef848304aaab08b74774706bd50680c39b5715b6
SultanIndian007 <amantiwari201608@gmail.com>
2023-12-31 00:02:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res) =>{
+    res.send('oopsie')
+})
+
+app.get('/login', (req,res) =>{
+    res.send('<h1>login kr chomu</h1>')
+})
+
+app.get('/chai', (req,res) =>{
+    res.send('<h2>youtube.com</h2>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
